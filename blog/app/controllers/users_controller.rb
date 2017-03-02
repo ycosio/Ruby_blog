9f94422a6102b8a4aefaf33900507df71f4ad346
Yair <ycosio@ucol.mx>
2017-03-02 10:50:14 -0600

    Models structure refactored

---
+++ blog/app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+   @user = User.new(user_params)
+
+   if @user.save
+     redirect_to @user
+   else
+     render 'new'
+   end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :last_name, :email, :pass)
+    end
+end
