0e529551f43571b044ce8f26b362fb15365c8e69
Yair <ycosio@ucol.mx>
2017-03-02 18:00:16 -0600

    Including devise gem to manage sessions and create a post by user

---
+++ blog/app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+
+
+  before_action :authenticate_user! , except: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+   @post = current_user.posts.build(post_params)
+
+   if @post.save
+     redirect_to @post
+   else
+     render 'new'
+   end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :user_id, :content)
+    end
+
+end
